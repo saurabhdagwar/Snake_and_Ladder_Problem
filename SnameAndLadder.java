a0b0cb97fd9ff8bca08239ac7ece1989d998f2d0
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-11 00:38:04 +0530

    [Saurabh] added: Repeat the game till player won

--- SnameAndLadder.java
+++ SnameAndLadder.java
@@ -5,33 +5,42 @@ class SnakeAndLadder
 	static final int  STARTING_POSITION = 0;
 	static final int	ENDING_POSITION = 100;
 
-	public static void GamePlayed()
+	public void GamePlayed()
 	{
 //UC2 Initializing Rolling Dice
 		Random random = new Random();
-		int current_position;
-		int DICE = (random.nextInt(6)+1);
+		int current_position = STARTING_POSITION ;
+// UC4 while loop for repet till player won
+		System.out.println("Game Played by the player");
+		System.out.println(" ");
+		while( current_position < ENDING_POSITION )
+		{
+			int DICE = (random.nextInt(6)+1);
 //UC3 defining Snake, Ladder or no play
-		int action = random.nextInt(3);
-		current_position = STARTING_POSITION;
-		System.out.println("Rolling Dice : "+DICE);
-		System.out.println("Action perform :"+action);
-		if(action == 1 && (current_position + DICE != ENDING_POSITION))
-		{
+			int action = random.nextInt(3);
 
-				current_position = current_position + DICE;
-				System.out.println("Player moves for Ladder");
-		}
-		else if(action == 2 && (current_position - DICE != STARTING_POSITION))
-		{
+			//System.out.println("Rolling Dice : "+DICE);
+			//System.out.println("Action perform :"+action);
+			if(action == 1 && (current_position + DICE <= ENDING_POSITION))
+			{
 
-				current_position = current_position - DICE;
-				System.out.println("Player moves for Snake");
+					current_position = current_position + DICE;
+			//		System.out.println("Player moves for Ladder");
+			}
+			else if(action == 2 && (current_position - DICE >= STARTING_POSITION))
+			{
+
+					current_position = current_position - DICE;
+			//		System.out.println("Player moves for Snake");
+			}
+			else
+			{
+			//		 System.out.println("No Play");
+			}
+			System.out.print(" "+current_position);
+
 		}
-		else
-		{
-				 System.out.println("No Play");
-		}
+		System.out.println(" ");
 
 	}
 
