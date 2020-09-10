6ea5ee2485160c0bba00d46d8da015af5ce475f4
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-10 22:06:29 +0530

    [Saurabh] added: Initialising starting and Ending Position

---
+++ SnameAndLadder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class SnakeAndLadder
+{
+//UC1 Initializing Starting position of game
+	static final STARTING_POSITION = 0;
+	static final ENDING_POSITION = 100;
+
+
+	public static void main(String args[])
+	{
+
+	}
+}
