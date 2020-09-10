eefbfba286723b80634948a19c099a5a423f93c9
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-10 23:46:03 +0530

    [Saurabh] added: Defined Snake, ladder and No play conditions

--- SnameAndLadder.java
+++ SnameAndLadder.java
@@ -2,24 +2,43 @@ import java.util.*;
 class SnakeAndLadder
 {
 //UC1 Initializing Starting position of game
-<<<<<<< HEAD
-	static final int STARTING_POSITION = 0;
-	static final int ENDING_POSITION = 100;
-=======
 	static final int  STARTING_POSITION = 0;
 	static final int	ENDING_POSITION = 100;
->>>>>>> UC2_Rolls_a_Die
 
-	public void Rolling_Dice()
+	public static void GamePlayed()
 	{
+//UC2 Initializing Rolling Dice
 		Random random = new Random();
+		int current_position;
 		int DICE = (random.nextInt(6)+1);
-		System.out.println("Rolling Dice :"+DICE);
+//UC3 defining Snake, Ladder or no play
+		int action = random.nextInt(3);
+		current_position = STARTING_POSITION;
+		System.out.println("Rolling Dice : "+DICE);
+		System.out.println("Action perform :"+action);
+		if(action == 1 && (current_position + DICE != ENDING_POSITION))
+		{
+
+				current_position = current_position + DICE;
+				System.out.println("Player moves for Ladder");
+		}
+		else if(action == 2 && (current_position - DICE != STARTING_POSITION))
+		{
+
+				current_position = current_position - DICE;
+				System.out.println("Player moves for Snake");
+		}
+		else
+		{
+				 System.out.println("No Play");
+		}
+
 	}
 
 	public static void main(String args[])
 	{
-		SnakeAndLadder Game1 = new SnakeAndLadder();
-		Game1.Rolling_Dice();
+	SnakeAndLadder	Player = new SnakeAndLadder();	
+		Player.GamePlayed();
 	}
 }
+
