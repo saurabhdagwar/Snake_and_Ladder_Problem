2345db4b2eeee2550e13f185a695e82d12b5650d
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-11 11:30:55 +0530

    [Saurabh] modified: Initializing a Rolling Dice

--- SnameAndLadder.java
+++ SnameAndLadder.java
@@ -2,13 +2,20 @@ import java.util.*;
 class SnakeAndLadder
 {
 //UC1 Initializing Starting position of game
-	static final int  STARTING_POSITION = 0;
-	static final int	ENDING_POSITION = 100;
+        static final int  STARTING_POSITION = 0;
+        static final int        ENDING_POSITION = 100;
 
+        public void Rolling_Dice()
+        {
+                Random random = new Random();
+                int DICE = (random.nextInt(6)+1);
+                System.out.println("Rolling Dice :"+DICE);
+        }
 
-	public static void main(String args[])
-	{
-	SnakeAndLadder	Player = new SnakeAndLadder();	
-	}
+        public static void main(String args[])
+        {
+                SnakeAndLadder Game1 = new SnakeAndLadder();
+                Game1.Rolling_Dice();
+        }
 }
 
