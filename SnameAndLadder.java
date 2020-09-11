5fdcdcbdbcb2a16d1ad1c8038a942188aa46f10d
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-11 11:46:50 +0530

    [Saurabh] modified: counted total times dice throws

--- SnameAndLadder.java
+++ SnameAndLadder.java
@@ -10,13 +10,15 @@ class SnakeAndLadder
 //UC2 Initializing Rolling Dice
 		Random random = new Random();
 		int current_position = STARTING_POSITION ;
-
+		String task ;
+		int total_dice_count = 0;
 // UC4 while loop for repet till player won
 		System.out.println("Game Played by the player");
 		System.out.println(" ");
 		while( current_position < ENDING_POSITION )
 		{
 			int DICE = (random.nextInt(6)+1);
+			total_dice_count++;
 //UC3 defining Snake, Ladder or no play
 			int action = random.nextInt(3);
 
@@ -26,24 +28,26 @@ class SnakeAndLadder
 			{
 
 					current_position = current_position + DICE;
+					task = "Ladder";
 			//		System.out.println("Player moves for Ladder");
 			}
 			else if(action == 2 && (current_position - DICE >= STARTING_POSITION))
 			{
 
 					current_position = current_position - DICE;
+					task = "Snake";
 			//		System.out.println("Player moves for Snake");
 			}
 			else
 			{
+					task = "noPlay";
 			//		 System.out.println("No Play");
 			}
-//UC5 Player reaches at exact winning position 
-			System.out.print(" "+current_position);
-
+			System.out.println(" | Dice: "+DICE+" for "+task+" *:"+current_position+"| ");
 		}
 		System.out.println(" ");
-
+//UC6 total times dice throws
+		System.out.println("Total Count of Dice Throws : "+total_dice_count);
 	}
 
 	public static void main(String args[])
