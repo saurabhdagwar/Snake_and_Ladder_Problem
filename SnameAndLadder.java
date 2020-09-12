1eaff1f0e5e4ad33bd7eca8bcf041c96d48aa5e8
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-12 14:40:10 +0530

    [Saurabh]Refactored: changes constant names

--- SnameAndLadder.java
+++ SnameAndLadder.java
@@ -2,8 +2,8 @@ import java.util.*;
 class SnakeAndLadder
 {
 //UC1 Initializing Starting position of game
-	static final int  STARTING_POSITION = 0;
-	static final int	ENDING_POSITION = 100;
+	static final int STARTING_POSITION = 0;
+	static final int ENDING_POSITION = 100;
 
 	public void GamePlayed()
 	{
