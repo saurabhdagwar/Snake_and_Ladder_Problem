fa978b4b43c1a508a6cf8baffbb04ea40488e6fa
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-16 13:47:27 +0530

    [Saurabh]Refactored: object of class

--- SnameAndLadder.java
+++ SnameAndLadder.java
@@ -4,10 +4,9 @@ class SnakeAndLadder
 //UC1 Initializing Starting position of game
 	static final int STARTING_POSITION = 0;
 	static final int ENDING_POSITION = 100;
-
-
 	public static void main(String args[])
 	{
-
+//Main method
+	SnakeAndLadder player = new SnakeAndLadder();
 	}
 }
